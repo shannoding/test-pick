6291870d0de6a6a12ccbe917634e01480ab94632
Shannon Ding <shannon_ding@yahoo.com>
2018-09-27 07:54:30 -0700

    Add files via upload

---
+++ chrome-test-202/scripts.js
@@ -0,0 +1,70 @@
+var tests = []; // array of objects with "label", "link", and "visible" 
+
+var form = document.getElementById("form");
+var submit = document.getElementById("submit");
+var options = document.getElementsByClassName("option");
+var selected;
+var prev;
+
+// tabletop code at https://github.com/jsoma/tabletop
+// gets rows of Google Sheet
+var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1R4PFHTE2PEGfS8twMUfrTl1Am71oCunR8dgh0LBNOJM/';
+// url must be set to viewable by everyone
+  function init() {
+    Tabletop.init( { key: publicSpreadsheetUrl,
+                     callback: showInfo,
+                     simpleSheet: true } )
+    // initializes Tabletop to get data and do the showInfo callback
+    console.log("done init");
+  }
+
+  function showInfo(data, tabletop) { // called after data from sheet is loaded
+	// data is an array of javascript objects (each representing a row) with the top row as keys and the target row as the values
+  	for (var i = 0; i < data.length; i++) { // takes each object in data and populates the tests array
+  		var testEntry = {
+  			"label": data[i]['Label'],
+  			"link": data[i]['Link'],
+  			"visible": (data[i]['Visible'].toLowerCase() === 'true') // boolean
+  		}
+  		tests.push(testEntry);
+  	}
+    console.log(data);
+    fillOptions();
+  }
+
+
+init(); // starts Tabletop
+
+// displays the filled tests array in the HTML 
+function fillOptions() {
+	var option;
+	for (var i = 0; i < tests.length; i++) {
+		if (tests[i].visible) {
+			option = document.createElement("div");
+			option.setAttribute("class", "option");
+			option.setAttribute("id", i);
+			option.appendChild(document.createTextNode(tests[i].label));
+			form.appendChild(option);
+			// adds a new div per object in tests
+			option.addEventListener("click", function() {
+				if (prev) {
+					prev.classList.remove("selected");
+				}
+				selected = this;
+				this.classList.toggle("selected");
+				prev = this;
+				console.log(this);
+			});
+		}
+	}
+	console.log("filled options");
+}
+
+
+// takes user to the correct link when the button is clicked
+submit.addEventListener("click", function() {
+	if (selected) {
+		window.location.href = tests[selected.id].link;
+		console.log("redirected");
+	}
+});
